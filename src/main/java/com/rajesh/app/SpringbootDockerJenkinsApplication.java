c532beadbba798276f84cebb2b2df45babd82041
Rajesh Bhola <rajeshbhola375@gmail.com>
2023-03-27 19:59:48 +0530

    second check for jenkins

--- src/main/java/com/rajesh/app/SpringbootDockerJenkinsApplication.java
+++ src/main/java/com/rajesh/app/SpringbootDockerJenkinsApplication.java
@@ -8,6 +8,7 @@ public class SpringbootDockerJenkinsApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Jenkins");
+		System.out.println("Second time checking Jenkins");
 		SpringApplication.run(SpringbootDockerJenkinsApplication.class, args);
 	}
 
